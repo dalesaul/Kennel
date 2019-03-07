a84351be98f72e19f43f0f0c34c2e632f32bbad9
Dale <dalesauljr@gmail.com>
2019-03-07 13:52:30 -0500

    refactored to use resource modules throughout

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,6 +4,10 @@ import Animals from './animal/Animals'
 import Location from './location/Location'
 import Employees from './employee/EmployeeList'
 import Owners from "./owners/Owners"
+import AnimalAPIManager from "../modules/AnimalManager"
+import EmployeeManager from '../modules/EmployeeManager'
+import OwnerManager from "../modules/OwnerManager"
+import LocationManager from "../modules/LocationManager"
 
 
 export default class ApplicationViews extends Component {
@@ -55,23 +59,25 @@ export default class ApplicationViews extends Component {
 
     componentDidMount(){
         const newState = {}
-        fetch("http://localhost:5002/employees")
-        .then(employees => employees.json())
+        return EmployeeManager.getAll()
         .then(parsedEmployees => {
             newState.employees = parsedEmployees;
-            return fetch("http://localhost:5002/locations")
-        }).then(locations => locations.json())
+            return LocationManager.getAll()
+        })
+
         .then(parsedLocations => {
             newState.locations = parsedLocations;
-            return fetch("http://localhost:5002/animals")
-            .then(animals => animals.json())
-        }).then(parsedAnimals => {
-            newState.animals = parsedAnimals;
-            return fetch("http://localhost:5002/owners")
-            .then(owners => owners.json())
-        }).then(parsedOwners => {
+            return OwnerManager.getAll()
+        })
+
+        .then(parsedOwners => {
             newState.owners = parsedOwners;
-        this.setState(newState);
+            return AnimalAPIManager.getAll()
+        })
+
+        .then(parsedAnimals => {
+            newState.animals = parsedAnimals;
+            this.setState(newState);
            })
         }
 
